9814618ebf2b4236f381f419e701d925c99d29cf
toothmonsterr <ssteffb@gmail.com>
2024-05-04 20:56:08 -0400

    ui tool to change brightness of textbox

--- scripts/Shader_bool.gd
+++ scripts/Shader_bool.gd
@@ -2,12 +2,21 @@
 extends ColorRect
 
 @export var AffectText : bool = true
+@export var darkness : float = 1.0 
+@onready var labeltex := %LabelTex
+@onready var texttex := %TextTex
 
+
+func darken(darkness, subject):
+	subject.modulate = Color.from_hsv(0,0,darkness,1)
+	
 func moveZ():
 	if AffectText:
 		set_z_index(1)
 	else: 
 		set_z_index(0)
 
-func _process(delta):
+func _process(_delta):
 	moveZ()
+	darken(darkness, labeltex)
+	darken(darkness, texttex)
